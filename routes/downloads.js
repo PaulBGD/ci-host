2d7d301edff125c3688d53e035924127af0ce792
Paul Sauve <paul@burngames.net>
2015-08-01 00:35:37 -0500

    Don't require logging in if it's already public

--- routes/downloads.js
+++ routes/downloads.js
@@ -1,37 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+var EMPTY_ARRAY = [];
+
 /* GET home page. */
 router.get('/*/*', function (req, res, next) {
-    if (req.session.logged_in) {
-        var split = req.url.substr(1).split('/');
-        var project = parseInt(split[0]);
-        var build = parseInt(split[1]);
-        if (isNaN(project) || isNaN(build)) {
-            var err = new Error('Invalid parameters');
-            err.status = 400;
-            return next(err);
-        }
-        var projectObj = projectManager.getProject(project);
-        if (!projectObj) {
-            err = new Error('Project does not exist');
-            err.status = 400;
-            return next(err);
-        }
-        if (req.session.ids.indexOf(project) === -1 && !projectObj.info.public) {
-            err = new Error('Permission denied');
-            err.status = 400;
-            return next(err);
-        }
-        if (build < 0 || projectObj.info.builds.length <= build) {
-            err = new Error('Build does not exist');
-            err.status = 400;
-            return next(err);
-        }
-        res.download(projectObj.info.builds[build].id);
-    } else {
-        res.render('log_in', {logged_in: false});
+    var split = req.url.substr(1).split('/');
+    var project = parseInt(split[0]);
+    var build = parseInt(split[1]);
+    if (isNaN(project) || isNaN(build)) {
+        var err = new Error('Invalid parameters');
+        err.status = 400;
+        return next(err);
     }
+    var projectObj = projectManager.getProject(project);
+    if (!projectObj) {
+        err = new Error('Project does not exist');
+        err.status = 400;
+        return next(err);
+    }
+    if ((req.session.ids || EMPTY_ARRAY).indexOf(project) === -1 && !projectObj.info.public) {
+        err = new Error('Permission denied');
+        err.status = 400;
+        return next(err);
+    }
+    if (build < 0 || projectObj.info.builds.length <= build) {
+        err = new Error('Build does not exist');
+        err.status = 400;
+        return next(err);
+    }
+    res.download(projectObj.info.builds[build].id);
 });
 
 module.exports = router;
