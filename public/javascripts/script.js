5e9c22db91878e0e1401a2cf9b218b0bf0e32b1a
Paul Sauve <paul@burngames.net>
2015-08-01 16:18:06 -0500

    No use for these

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,10 +1,1 @@
 $('[data-toggle="tooltip"]').tooltip();
-$('.download-select').change(function() {
-    var $this = $(this);
-    var val = $this.val();
-    var project = $this.attr('data-project');
-    var a = $this.parent().find('a');
-    var selected = $this.find('option:selected');
-    var id = selected.attr('data-id');
-    a.attr('href', '/downloads/' + project + '/' + val + '/' + id);
-});
